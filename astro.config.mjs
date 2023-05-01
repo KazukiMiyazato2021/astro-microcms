0813040a4189f750047ed60efd4dfa0471070739
KazukiMiyazato2021 <kazuki.miyazato.2021+git@gmail.com>
2023-05-01 18:17:22 +0900

    ➕ add @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://KazukiMiyazato2021.github.io/astro-microcms',
   base: '/astro-microcms',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), image()],
   vite: {
-    base: '/astro-microcms',
-  },
+    base: '/astro-microcms'
+  }
 });
